5b87ae56a3dcf113dcee3cc8c02c7b0480a4ad27
spayuso <spayuso@gmail.com>
2015-10-10 11:26:10 +0200

    Create plot1.R
    Plot 1

---
+++ plot1.R
@@ -0,0 +1,13 @@
+#Exploratory data Analysis Assignment 1
+#Plot 1 
+#Sebastian Pajuelo
+
+print("Reading proccess")
+data<-read.table("/Users/Sebastian/Desktop/VARIOS/workspace/household_power_consumption.txt", header=T, sep=";", na.strings="?")
+procData<-data[data$Date %in% c("1/2/2007","2/2/2007"),]
+
+time<-strptime(paste(procData$Date, procData$Time, sep=""),"%d/%m/%Y %H:%M:%S")
+procData<-cbind(time,procData)
+png("plot1.png", width=480, height=480)
+hist(procData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kW)")
+dev.off()
